c12fa54bca7b786b8abc6e2d6da988e85274bdae
Hannes Becher <hannesbecher@gmx.de>
2025-10-10 16:42:19 +0100

    julia works, performance like R/spam

--- rppcajl/RandPedPCA/src/RandPedPCA.jl
+++ rppcajl/RandPedPCA/src/RandPedPCA.jl
@@ -1,18 +1,47 @@
 module RandPedPCA
 
+
+# TODO:
+# - centring
+
 # Write your package code here.
 using LinearAlgebra
 using DelimitedFiles
 
-export ped2Li
+export ped2Li, rangeFinder, rSVD, rppca
 
 function ocarleLi(li, x)
-    return(solve(li, x))
-
+    return(li \ (li' \ x))
 end # function ocarleLi
 
+function rangeFinder(li, rank, depth, numVectors)
+    n = size(li, 1)
+    Ω = randn(n, numVectors)
+    Y = li * Ω
+    
+    Q = Matrix(qr(Y).Q)
+    for i in 2:depth
+        Y = ocarleLi(li, Q)
+        Q = Matrix(qr(Y).Q)
+    end
+
+    return Q[:, 1:rank]
+end # function rangeFinder
 
 
 
+function rSVD(li, rank, depth, numVectors)
+    Q = rangeFinder(li, rank, depth, numVectors)
+    B = (li' \ Q)'
+    U, S, V = svd(B)
+    UU = Q * U
+    return UU[:,1:rank], S[1:rank], V[:,1:rank]
+end # function rSVD
+
+
+function rppca(x, rank, depth, numVectors)
+    U,S,_ = rSVD(x, rank, depth, numVectors)
+    return U * diagm(S.^2), S
+end # function rppca
 
 end # module
--- rppcajl/RandPedPCA/test/runtests.jl
+++ rppcajl/RandPedPCA/test/runtests.jl
@@ -2,7 +2,7 @@ using RandPedPCA
 using Test
 using DelimitedFiles
 using MatrixMarket
-
+using LinearAlgebra
 
 # @testset "RandPedPCA.jl" begin
 #     # Write your tests here.
@@ -13,8 +13,23 @@ using MatrixMarket
 # end
 # 
 
-@testset "Import sparse matris in MM format" begin
+@testset "Import sparse matrix in MM format" begin
+    rr = randn(2650)
     @test_nowarn li = MatrixMarket.mmread("li2.mtx")
-    
+    li = MatrixMarket.mmread("li2.mtx")
+    ai = li' * li
+    @test all(li \ (li' \ rr) .≈ ai \ rr)
     
 end
+
+@testset "Range finder" begin
+    li = MatrixMarket.mmread("li2.mtx")
+    Q = rangeFinder(li, 10, 2, 20)
+    
+    @test size(Q) == (size(li, 1), 10)
+    @test_nowarn rSVD(li, 10, 2, 20)
+
+end
+
+
+
